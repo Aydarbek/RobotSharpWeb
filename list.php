7e8ff41e3bb0f03353fa27805fc27b174589a295
Jevgenij Volosatov <fformula@gmail.com>
2019-01-21 15:37:40 +0200

    Robot Web First Commit withDesign

---
+++ list.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+        <script src="js/php.TaskList.js"></script>
+        <script src="js/stuff.js"></script>
+        <script src="js/run.list.js"></script>
+
+        <title>Список задач</title>
+    </head>
+    <body>
+        <div class="navbar mb-4 text-white bg-success">
+			<h1 id="htmlTitle">Список задач</h1>
+		</div>
+		<div class="container-fluid px-4">
+			<table id="table" class="table table-hover table-bordered">
+				<thead class="thead-dark">
+					<tr>
+						<th class="text-center">№</th>
+						<th>Название задачи</th>
+					</tr> 
+				</thead>
+				<tbody>
+					<tr>
+						<td class="text-center">
+							<span class="badge badge-primary badge-pill">$taskId</span> 
+						</td>
+						<td>
+							<a href="task.html?taskId=$taskId" title="Открыть условие">
+								$title
+							</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+    </body>
+</html>
