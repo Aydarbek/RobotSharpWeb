9a31f53dcc939883e9fa2d765caada6ea821b6b4
Jevgenij Volosatov <fformula@gmail.com>
2019-02-01 11:54:57 +0200

    user login from partner site

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+    sleep(1);
+    $partner = $_GET['partner'];
+    $apikey = 'xxx';
+    $time = $_GET['time'];
+    $email = $_GET['email'];
+    $name = $_GET['name'];
+    $signUser = $_GET['sign'];
+    $signHost = md5("$partner/$apikey/$time/$email");
+
+    if ($signHost != $signUser)
+        die('Sign incorrect');
+
+    session_start();
